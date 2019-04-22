30a4cb1607754d4b879410f1a4175712a40becd4
ChengZu <1351606745@msn.cn>
2019-04-22 15:48:54 +0800

    Initial commit
    Initial commit

---
+++ www/reg.php
@@ -0,0 +1,47 @@
+<?php
+if(!isset($_GET['code'])){
+	die();
+}
+
+$macid=$_GET['code'];
+$time=time()+24*60*60;
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$mysql_conf = array(
+    'host'    => 'sql310.epizy.com:3306', 
+    'db'      => 'epiz_22444804_wowfish', 
+    'db_user' => 'epiz_22444804', 
+    'db_pwd'  => '6354050', 
+    );
+
+$mysqli = @new mysqli($mysql_conf['host'], $mysql_conf['db_user'], $mysql_conf['db_pwd']);
+if ($mysqli->connect_errno) {
+    die();//诊断连接错误
+}
+$mysqli->query("set names 'utf8';");//编码转化
+$select_db = $mysqli->select_db($mysql_conf['db']);
+if (!$select_db) {
+    die();
+}
+
+$sql = "select time from regdata where macid = '$macid';";
+$res = $mysqli->query($sql);
+
+
+if (!$res) {
+    die();
+}
+
+if ($row = $res->fetch_assoc()) {
+    echo time().','.$row['time'];
+}else{
+    $sql = "INSERT INTO regdata (macid,time,num,ip) VALUES('$macid','$time','0','$ip');";
+    $res = $mysqli->query($sql);
+    echo time().','.$time;
+}
+
+
+$res->free();
+$mysqli->close();
+
+?>
---
+++ www/wowfish.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.8.2
+-- http://www.phpmyadmin.net
+--
+-- Host: sql310.epizy.com
+-- Generation Time: Apr 06, 2019 at 06:09 AM
+-- Server version: 5.6.41-84.1
+-- PHP Version: 5.3.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `epiz_22444804_wowfish`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `regdata`
+--
+
+CREATE TABLE IF NOT EXISTS `regdata` (
+  `macid` varchar(32) NOT NULL,
+  `time` varchar(11) NOT NULL,
+  `num` int(11) NOT NULL,
+  `ip` varchar(15) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
