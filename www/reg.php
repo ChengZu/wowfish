9c74c863b814e21ca76d6b5e5ed7b397a354d9aa
ChengZu <1351606745@msn.cn>
2019-04-22 15:53:25 +0800

    Initial commit
    Initial commit

--- www/reg.php
+++ www/reg.php
@@ -16,9 +16,9 @@ $mysql_conf = array(
 
 $mysqli = @new mysqli($mysql_conf['host'], $mysql_conf['db_user'], $mysql_conf['db_pwd']);
 if ($mysqli->connect_errno) {
-    die();//诊断连接错误
+    die();
 }
-$mysqli->query("set names 'utf8';");//编码转化
+$mysqli->query("set names 'utf8';");
 $select_db = $mysqli->select_db($mysql_conf['db']);
 if (!$select_db) {
     die();
