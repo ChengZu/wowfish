152b6ac01b6bdd340daab2c84dae733e33c013f0
ChengZu <1351606745@msn.cn>
2019-04-22 16:09:27 +0800

    Initial commit
    Initial commit

--- www/wowfish.sql
+++ www/wowfish.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `epiz_22444804_wowfish`
+-- Database: `database_wowfish`
 --
 
 -- --------------------------------------------------------
